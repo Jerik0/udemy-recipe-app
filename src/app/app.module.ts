57c25150ad9cea6dbf947f38afb510df3389f059
jerik0 <jcarrico1985@live.com>
2020-07-10 17:13:56 -0500

    update header section and update recipe section with dummy data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ShoppingListComponent } from './components/shopping/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './components/shopping/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/recipe/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
+import { ShoppingListEditComponent } from './components/shopping/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/recipe/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipe/recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './components/recipe/recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-
     ShoppingListEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
